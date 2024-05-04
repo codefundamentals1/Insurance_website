66e4075ca7d3b970644f4ee80d0cb21f8b5286e5
codefundamentals1 <anishr888999@gmail.com>
2024-05-04 07:46:56 +0530

    Configured .env

--- Backend/src/db/config.js
+++ Backend/src/db/config.js
@@ -1,11 +1,14 @@
 const mysql = require("mysql2");
+require('dotenv').config()
+const dotenv = require("dotenv").config();
+
 
 //this is the syntax to connect my sqll
 const con = mysql.createConnection({
-  host: 'localhost',
-  user: "raja",
-  password: "Anish@123",
-  database: "users",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 con.connect((err) => {
--- Backend/src/db/configmain.js
+++ Backend/src/db/configmain.js
@@ -1,14 +1,18 @@
 const mysql = require("mysql2");
+require('dotenv').config()
+const dotenv = require("dotenv").config();
+
 
 //this is the syntax to connect my sqll
 const mydata = mysql.createConnection({
-  host: 'localhost',
-  user: "raja",
-  password: "Anish@123",
-  database: "DBMS",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_MAIN_DATABASE,
 });
 
-mydata.connect((err) => {
+
+mydata.connect((err) => { 
   if (err) {
     console.log(err);
   } else {  
